ee97702166196e87b576c4a417282e79f7c650ab
Marcello Farias <marcellorocha632@gmail.com>
2024-02-15 11:52:38 -0300

    Add movie details

--- src/pages/Movie/movie.jsx
+++ src/pages/Movie/movie.jsx
@@ -17,7 +17,7 @@ function Movie() {
     return (
         <>
             <main className="movie-details-container movie-banner-container">
-                <img src={"https://image.tmdb.org/t/p/original" + movie?.poster_path} alt="movie-poster" className="movie-poster" />
+                <img src={"https://image.tmdb.org/t/p/original" + movie?.backdrop_path} alt="movie-poster" className="movie-backdrop" />
                 <div className="movie-title">
                     <h1>{movie?.title}</h1>
                     <p>{movie?.tagline}</p>
@@ -34,7 +34,20 @@ function Movie() {
             </div>
 
             <section className="movie-details-container">
-                
+                <div className="movie-poster-container">
+                    <img src={"https://image.tmdb.org/t/p/original" + movie?.poster_path} alt="movie-poster" className="movie-poster"/>                                         
+                </div>
+                <div className="movie-details">
+                    <p className="movie-detail">Duration - {movie?.runtime} min</p>
+                    <p className="movie-detail">Release - {movie?.release_date.replaceAll("-", "/")}</p>
+                    <p className="movie-detail">Rating - {movie?.vote_average.toFixed(2)}</p>
+                    <div className="movie-genres">
+                        <span>Genres</span>
+                        {movie?.genres.map((genre) => {
+                            return <p className="movie-detail" key={genre.id}>{genre.name}</p>
+                        })}
+                    </div>
+                </div>
             </section>
         </>
     );
